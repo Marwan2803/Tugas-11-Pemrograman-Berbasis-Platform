77afc494b9dd4cc7074ed25f8e15fdddb233a1cc
Marwan2803 <marwanmuharam207@gmail.com>
2024-12-20 01:10:59 +0700

    Update dbprisma.js

--- Prisma/dbprisma.js
+++ Prisma/dbprisma.js
@@ -1,5 +1,63 @@
-const { PrismaClient } = require("@prisma/client");
+generator client {
+  provider = "prisma-client-js"
+}
 
-const prisma = new PrismaClient();
+datasource db {
+  provider = "mysql"
+  url      = env("DATABASE_URL")
+}
 
-module.exports = prisma;
+model ligabelanda {
+  id     Int    @id @default(autoincrement())
+  nama   String @db.VarChar(50)
+  umur   Int
+  posisi String @db.VarChar(4)
+  NA     String @db.VarChar(50)
+  KA     String @db.VarChar(100)
+  KSI    String @db.VarChar(100)
+  harga  String @db.VarChar(100)
+}
+
+model ligainggris {
+  id     Int    @id @default(autoincrement())
+  nama   String @db.VarChar(50)
+  umur   Int
+  posisi String @db.VarChar(4)
+  NA     String @db.VarChar(50)
+  KA     String @db.VarChar(100)
+  KSI    String @db.VarChar(100)
+  harga  String @db.VarChar(100)
+}
+
+model ligajerman {
+  id     Int    @id @default(autoincrement())
+  nama   String @db.VarChar(50)
+  umur   Int
+  posisi String @db.VarChar(4)
+  NA     String @db.VarChar(50)
+  KA     String @db.VarChar(100)
+  KSI    String @db.VarChar(100)
+  harga  String @db.VarChar(100)
+}
+
+model ligaprancis {
+  id     Int    @id @default(autoincrement())
+  nama   String @db.VarChar(50)
+  umur   Int
+  posisi String @db.VarChar(4)
+  NA     String @db.VarChar(50)
+  KA     String @db.VarChar(100)
+  KSI    String @db.VarChar(100)
+  harga  String @db.VarChar(100)
+}
+
+model ligaspanyol {
+  id     Int    @id @default(autoincrement())
+  nama   String @db.VarChar(50)
+  umur   Int
+  posisi String @db.VarChar(4)
+  NA     String @db.VarChar(50)
+  KA     String @db.VarChar(100)
+  KSI    String @db.VarChar(100)
+  harga  String @db.VarChar(100)
+}
